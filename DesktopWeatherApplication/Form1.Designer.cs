c4aaf58fef3cfd355d0934027b62c6d6b16619dc
BJ Dedushaj <49247072+bjded@users.noreply.github.com>
2021-05-27 23:37:49 -0500

    Add files via upload

---
+++ DesktopWeatherApplication/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace DesktopWeatherApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(136, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check Weather";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 72);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(260, 277);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Info;
+            this.button2.Location = new System.Drawing.Point(244, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter ZIP Code (US)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MinimumSize = new System.Drawing.Size(300, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "US Weather App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DesktopWeatherApplication/Form1.cs
@@ -0,0 +1,94 @@
+﻿using DesktopWeatherApplication.models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopWeatherApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void getWeather(string url, string zipcode)
+        {
+            using (WebClient web = new WebClient())
+            {
+                var json = web.DownloadString(url);
+                var result = JsonConvert.DeserializeObject<WeatherInformation.Root>(json);
+                WeatherInformation.Root data = result;
+
+                richTextBox1.Text += "ZIP Code: " + zipcode + "\n";
+                richTextBox1.Text += "City: " + data.location.name + "\n";
+                richTextBox1.Text += "State: " + data.location.region + "\n";
+                richTextBox1.Text += "Country: " + data.location.country + "\n";
+                richTextBox1.Text += "Latitude: " + data.location.lat + "\n";
+                richTextBox1.Text += "Longitude: " + data.location.lon + "\n";
+                richTextBox1.Text += "Time Zone: " + data.location.tz_id + "\n";
+                richTextBox1.Text += "Local Time: " + data.location.localtime + "\n";
+                richTextBox1.Text += "Updated: " + data.current.last_updated + "\n";
+                richTextBox1.Text += "Celsius: " + data.current.temp_c + "\n";
+                richTextBox1.Text += "Fahrenheit: " + data.current.temp_f + "\n";
+                richTextBox1.Text += "Wind (MPH): " + data.current.wind_mph + "\n";
+                richTextBox1.Text += "Wind (KPH): " + data.current.wind_kph + "\n";
+                richTextBox1.Text += "Wind Degree: " + data.current.wind_degree + "\n";
+                richTextBox1.Text += "Wind Direction: " + data.current.wind_dir + "\n";
+                richTextBox1.Text += "Pressure (MB): " + data.current.pressure_mb + "\n";
+                richTextBox1.Text += "Pressure (IN): " + data.current.pressure_in + "\n";
+                richTextBox1.Text += "Precipitation (MM): " + data.current.precip_mm + "\n";
+                richTextBox1.Text += "Precipitation (IN): " + data.current.precip_in + "\n";
+                richTextBox1.Text += "Humidity: " + data.current.humidity + "\n";
+                richTextBox1.Text += "Cloud: " + data.current.cloud + "\n";
+                richTextBox1.Text += "Feels Like (C): " + data.current.feelslike_c + "\n";
+                richTextBox1.Text += "Feels Like (F): " + data.current.feelslike_f + "\n";
+                richTextBox1.Text += "Visibility (KM): " + data.current.vis_km + "\n";
+                richTextBox1.Text += "Visibility (MI): " + data.current.vis_miles + "\n";
+                richTextBox1.Text += "UV: " + data.current.uv + "\n";
+                richTextBox1.Text += "Gust (MPH): " + data.current.gust_mph + "\n";
+                richTextBox1.Text += "Gust (KPH): " + data.current.gust_kph + "\n";
+                richTextBox1.Text += "Air Quality (co): " + data.current.air_quality.co + "\n";
+                richTextBox1.Text += "Air Quality (no2): " + data.current.air_quality.no2 + "\n";
+                richTextBox1.Text += "Air Quality (o3): " + data.current.air_quality.o3 + "\n";
+                richTextBox1.Text += "Air Quality (so2): " + data.current.air_quality.so2 + "\n";
+                richTextBox1.Text += "Air Quality (pm2_5): " + data.current.air_quality.pm2_5 + "\n";
+                richTextBox1.Text += "Air Quality (pm10): " + data.current.air_quality.pm10 + "\n";
+                richTextBox1.Text += "Air Quality (us-epa-index): " + data.current.air_quality.UsEpaIndex + "\n";
+                richTextBox1.Text += "Air Quality (gb-defra-index): " + data.current.air_quality.GbDefraIndex + "\n";
+                richTextBox1.Text += "\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // variables
+            string uniqueKey = "a884043157774396add153753212705";
+            string getZipCode = "";
+
+            // get zip code
+            getZipCode = textBox1.Text;
+
+            // create string for api call
+            string apiCall = string.Concat("https://api.weatherapi.com/v1/current.json?key=", uniqueKey, "&q=", getZipCode, "&aqi=yes");
+
+            // get weather
+            getWeather(apiCall, getZipCode);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // clear all
+            textBox1.Text = "";
+            richTextBox1.Text = "";
+        }
+    }
+}
